261e071bb9ab5925c60dee052e6e5d8119c13309
Riccardo Balbo <riccardo0blb@gmail.com>
2024-01-03 15:55:53 +0100

    Add more common errors

--- src/js/Errors.js
+++ src/js/Errors.js
@@ -1,3 +1,5 @@
 export default {
     "bad-txns-fee-outofrange": "Transaction fee is out of range",
+    "bad-txns-fee-negative": "Transaction fee is negative",
+    "bad-txns-in-belowout": "Transaction is invalid! Input value is below output value",
 };
